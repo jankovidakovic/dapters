9adf348ba935bfa18bbb5ac6a61dfde7e919a925
jvidakovic <janko.vidakovic@gmail.com>
2023-04-07 21:42:13 +0200

    Remove unused args and add TODOs to implement

--- src/finetuning/cli.py
+++ src/finetuning/cli.py
@@ -113,37 +113,12 @@ def get_parser():
         help="If set, input text will be lowercased during tokenization. "
              "This flag is useful when one is using uncased models (e.g. 'bert-base-uncased')",
     )
-    parser.add_argument(
-        "--max_seq_length",
-        default=None,
-        type=int,
-        help="Maximum sequence length after tokenization, i.e. maximum "
-             "number of tokens that one data example should consist of. "
-             "Sequences with less tokens will be padded, sequences with "
-             "more tokens will be truncated. If not provided, defaults to "
-             "the maximum sequence length allowed by the model (which is "
-             "specified by '--pretrained_model_name_or_path')."
-    )
 
-    parser.add_argument(
-        "--per_device_train_batch_size",
-        default=2,
-        type=int,
-        help="Batch size used during training (per device). Defaults to 2.",
-    )
     parser.add_argument(
         "--per_device_eval_batch_size",
         default=2,
         type=int,
         help="Batch size used during evaluation (per device). Defaults to 2.",
-    )
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help="Number of updates steps to accumulate before performing an "
-             "update to the model's parameters. Defaults to 1. Useful for "
-             "using batch sizes bigger than allowed by the available GPU memory."
     )
     parser.add_argument(
         "--learning_rate",
@@ -152,34 +127,10 @@ def get_parser():
         help="The initial learning rate used for optimization. Defaults to 1e-5.",
     )
     parser.add_argument(
-        "--lr_scheduler_type",
-        default="linear",
-        choices=["linear", "linear_halve", "cosine",
-                 "cosine_hard_restart", "constant"],
-        type=str,
-        help="The learning rate schedule type for Adam. Defaults to linear",
-    )
-    parser.add_argument(
         "--weight_decay",
         default=0.0,
         type=float,
         help="Weight decay factor. Defaults to 0 (no weight decay)."
-    )
-    parser.add_argument(
-        "--warmup_steps",
-        default=0,
-        type=int,
-        help="Amount of steps during which the learning rate will be linearly increased. "
-             "Defaults to 0 (no warmup).",
-
-    )
-    parser.add_argument(
-        "--warmup_ratio",
-        default=0,
-        type=float,
-        help="Proportion of total number of training steps during which the learning rate "
-             "will be linearly increased. Useful when you don't know the exact number of steps,"
-             "but still want to warmup e.g. for first 10% of steps. Defaults to 0 (no warmup).",
     )
     parser.add_argument(
         "--adam_epsilon",
@@ -194,26 +145,14 @@ def get_parser():
         type=float,
         help="Maximum value of L2-norm of the gradients during optimization. Gradients "
              "with norm greater than this value will be clipped. Defaults to 1.0."
-    )
+    )  # TODO!!!!!!!!!!!!!!!!
 
-    parser.add_argument(
-        "--logging_steps",
-        type=int,
-        default=50,
-        help="Logging interval in steps. Defaults to 50."
-    )
-    parser.add_argument(
-        "--eval_steps",
-        type=int,
-        default=None,
-        help="Evaluation interval in steps. If unset, evaluatiion will not be performed."
-    )
     parser.add_argument(
         "--save_steps",
         type=int,
         default=None,
         help="Saving interval in steps. Defaults to None.",
-    )
+    )  # TODO!!!!!!!!!1
     parser.add_argument(
         "--save_total_limit",
         type=int,
@@ -224,40 +163,20 @@ def get_parser():
     parser.add_argument(
         "--metric_for_best_model",
         type=str,
-        required=True,
+        required=False, # TODO!!!!
         choices=["f1_macro", "precision", "recall", "loss"],
         help="Metric used to compare model checkpoints. Checkpoints will be sorted according "
              "to the value of provided metric on the development sets."
-    )
-    parser.add_argument(
-        "--dataloader_num_workers",
-        type=int,
-        default=0,
-        help="Number of workers to use for dataloading. For best performance, set "
-             "this to the number of available CPU cores."
     )
 
     # directories relevant for IO
     parser.add_argument(
         "--output_dir",
         type=str,
-        required=True,
+        required=False,
         help="The output directory where the model predictions and "
              "checkpoints will be written."
-    )
-    parser.add_argument(
-        "--cache_dir",
-        default="",
-        type=str,
-        help="Path to a directory containing cached models (downloaded from hub)."
-    )
-    parser.add_argument(
-        "--log_dir",
-        type=str,
-        default="./logs",
-        help="Directory used to store the run logs. Defaults to './logs', relative "
-             "to the working directory."
-    )
+    )  # TODO
 
     # misc
     parser.add_argument(  # TODO - make use of this
@@ -270,7 +189,7 @@ def get_parser():
     parser.add_argument(
         "--mlflow_experiment",
         type=str,
-        required=True,
+        required=False, # TODO
         help="Experiment name in MLFLow. If not provided, will not use MLFlow."
     )
 
