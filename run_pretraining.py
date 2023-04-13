1f443d41b5c6d7472f14cb72f8935cb35413bc2d
jvidakovic <jvidakovic@realnetworks.com>
2023-04-13 16:13:51 +0200

    Add pretraining script

---
+++ run_pretraining.py
@@ -0,0 +1,112 @@
+import logging
+
+import mlflow
+import pandas as pd
+from transformers import set_seed, DataCollatorForLanguageModeling, AutoModelForMaskedLM
+import torch
+
+from src.cli.pretraining import PreTrainingArguments, parse_args
+from src.data import setup_dataloaders
+from src.preprocess import pretraining_pipeline
+from src.trainer import train, evaluate_pretraining, pretraining_loss
+from src.utils import setup_logging, maybe_tf32, get_tokenizer, get_tokenization_fn, pipeline, setup_optimizers
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    # setup logging
+    args: PreTrainingArguments = parse_args()
+    setup_logging(args)
+    set_seed(args.random_seed)
+    maybe_tf32(args)
+
+    tokenizer = get_tokenizer(args)
+
+    do_tokenize = get_tokenization_fn(
+        tokenizer=tokenizer,
+        padding=args.padding,  # noqa
+        truncation=True,
+        max_length=args.max_length,
+        return_special_tokens_mask=True,
+    )  # would be cool to abstract this also
+
+    do_preprocess = pipeline(
+        pd.read_csv,
+        pretraining_pipeline(
+            initial_preprocessing=args.preprocessing,
+            tokenizing_fn=do_tokenize,
+    ))
+
+    train_dataset = do_preprocess(args.train_dataset_path)
+    eval_dataset = do_preprocess(args.eval_dataset_path)
+
+    # seems to be working!!
+    train_dataloader, eval_dataloader = setup_dataloaders(
+        train_dataset,
+        eval_dataset,
+        args,
+        collate_fn=DataCollatorForLanguageModeling(
+            tokenizer=tokenizer,
+            mlm=True,
+            mlm_probability=args.mlm_probability,
+        )
+    )
+
+    # initialize model
+    model = AutoModelForMaskedLM.from_pretrained(
+        args.pretrained_model_name_or_path,
+        cache_dir=args.cache_dir,
+    )
+
+    # compile model
+    model: PreTrainedModel = torch.compile(model).to(args.device)  # noqa
+
+    logger.info(f"Model loaded successfully on device: {model.device}")
+
+    # TODO - make configurable
+    optimizer, scheduler = setup_optimizers(
+        model,
+        lr=args.learning_rate,
+        weight_decay=args.weight_decay,
+        adam_epsilon=args.adam_epsilon,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        warmup_percentage=args.warmup_percentage,
+        epochs=args.epochs,
+        epoch_steps=len(train_dataloader),
+        scheduler_type=args.scheduler_type
+    )   # TODO - dataloader was an IterableDataset, we wouldnt have len -> fix
+
+    mlflow.set_tracking_uri("http://localhost:34567")
+    mlflow.set_experiment(args.mlflow_experiment)
+
+    mlflow.log_params(vars(args))
+
+    train(
+        model=model,
+        tokenizer=tokenizer,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        train_dataloader=train_dataloader,
+        eval_dataloader=eval_dataloader,
+        epochs=args.epochs,
+        eval_steps=args.eval_steps,
+        logging_steps=args.logging_steps,
+        save_steps=args.save_steps,
+        output_dir=args.output_dir,
+        max_grad_norm=args.max_grad_norm,
+        early_stopping_patience=args.early_stopping_patience,
+        metric_for_best_model=args.metric_for_best_model,
+        greater_is_better=args.greater_is_better,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        do_evaluate=evaluate_pretraining(),
+        get_loss=pretraining_loss(),
+    )
+
+    logger.warning("Training complete.")
+
+    mlflow.end_run()
+
+
+if __name__ == "__main__":
+    main()
