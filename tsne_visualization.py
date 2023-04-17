7fcd9d7690fc3bb5c740e5b51ef371acd3c6fe48
jvidakovic <jvidakovic@realnetworks.com>
2023-04-17 13:08:42 +0200

    Fix runtime errors

--- tsne_visualization.py
+++ tsne_visualization.py
@@ -7,6 +7,7 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
+import torch
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
 from torch.utils.data import DataLoader
