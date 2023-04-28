12abc7f383ecbf8da54293bd09af729541a15009
jvidakovic <jvidakovic@realnetworks.com>
2023-04-28 11:30:59 +0200

    Add dataset deduplication and remove neccessary eval dataset

--- run_finetuning.py
+++ run_finetuning.py
@@ -3,14 +3,16 @@ import logging
 import pandas as pd
 import torch
 from pandas import DataFrame
+from torch.utils.data import DataLoader
 from transformers import (
     set_seed,
-    AutoModelForSequenceClassification,
+    AutoModelForSequenceClassification, DefaultDataCollator,
 )
 
 from src.data import setup_dataloaders
 from src.cli.finetuning import FineTuningArguments, parse_args
-from src.preprocess.steps import multihot_to_list, to_hf_dataset, hf_map, convert_to_torch, sequence_columns
+from src.preprocess.steps import multihot_to_list, to_hf_dataset, hf_map, convert_to_torch, sequence_columns, \
+    deduplication
 from src.trainer import train, fine_tuning_loss, eval_loss_only
 from src.utils import setup_logging, get_labels, get_tokenization_fn, setup_optimizers, maybe_tf32, get_tokenizer, \
     pipeline, mean_binary_cross_entropy
@@ -37,11 +39,13 @@ def main():
         max_length=args.max_length,
         message_column=args.message_column
     )
+
     labels = get_labels(args.labels_path)
 
     do_preprocess = pipeline(
         pd.read_csv,
         DataFrame.dropna,
+        deduplication(args.message_column),
         multihot_to_list(
             label_columns=labels,
             result_column="labels"
@@ -55,12 +59,21 @@ def main():
 
     # we need to remove evaluation
 
-    train_dataset = do_preprocess(args.train_dataset_path)
-    eval_dataset = do_preprocess(args.eval_dataset_path)
+    # train_dataset = do_preprocess(args.train_dataset_path)
+    dataset = do_preprocess(args.train_dataset_path)
+    # if args.eval_dataset_path:
+        # eval_dataset = do_preprocess(args.eval_dataset_path)
 
     # TODO - IterableDataset = ?
-    train_dataloader, eval_dataloader = setup_dataloaders(
-        train_dataset, eval_dataset, args
+    # train_dataloader, eval_dataloader = setup_dataloaders(
+        # train_dataset, eval_dataset, args
+    # )
+    dataloader = DataLoader(
+        dataset,
+        batch_size=args.per_device_train_batch_size,
+        shuffle=True,
+        pin_memory=True,
+        collate_fn=DefaultDataCollator(return_tensors="pt")
     )
 
     # initialize model
@@ -85,34 +98,33 @@ def main():
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         warmup_percentage=args.warmup_percentage,
         epochs=args.epochs,
-        epoch_steps=len(train_dataloader),
+        epoch_steps=len(dataloader),
         scheduler_type=args.scheduler_type
     )   # TODO - dataloader was an IterableDataset, we wouldnt have len -> fix
 
     # set up mlflow
-    mlflow.set_tracking_uri(args.mlflow_tracking_uri)
-    mlflow_experiment = mlflow.set_experiment(args.mlflow_experiment)
+    if args.mlflow_experiment:
+        mlflow.set_tracking_uri(args.mlflow_tracking_uri)
+        mlflow_experiment = mlflow.set_experiment(args.mlflow_experiment)
 
-    mlflow.start_run(
-        experiment_id=mlflow_experiment.experiment_id,
-        run_name=args.mlflow_run_name,
-        description=args.mlflow_run_description
-    )
+        mlflow.start_run(
+            experiment_id=mlflow_experiment.experiment_id,
+            run_name=args.mlflow_run_name,
+            description=args.mlflow_run_description
+        )
 
-    mlflow.log_params(vars(args))
+        mlflow.log_params(vars(args))
 
     train(
         model=model,
         tokenizer=tokenizer,
         optimizer=optimizer,
         scheduler=scheduler,
-        train_dataloader=train_dataloader,
-        eval_dataloader=eval_dataloader,
+        train_dataloader=dataloader,
         epochs=args.epochs,
         logging_steps=args.logging_steps,
         output_dir=args.output_dir,
-        max_grad_norm=args.max_grad_norm,
-        do_evaluate=eval_loss_only(loss_fn=mean_binary_cross_entropy),
+        max_grad_norm=args.max_grad_norm,  # i guess no?
         get_loss=fine_tuning_loss(loss_fn=mean_binary_cross_entropy),
         early_stopping_patience=args.early_stopping_patience,
         metric_for_best_model=args.metric_for_best_model,
